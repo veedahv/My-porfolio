f9885201cc08b37d018a5051fb86ff67ee373875
veedahv <veedahv55@gmail.com>
2020-10-22 13:27:02 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-const hamburger = document.querySelector('.hamburger'),
-    header = document.querySelector('header'),
-    mobileNav = document.querySelector('.mobile-nav');
-hamburger.addEventListener('click', function (event) {
-          hamburger.classList.toggle("is-active");
-          mobileNav.classList.toggle("show");
+const hamburger = document.querySelector('#hamburger'),
+ hamburgerBtn = document.querySelector('#hamburger-btn'),
+  hamburgerClass = document.querySelector('.hamburger'),
+  mobileNavLinks = document.querySelectorAll('.mobile-nav-link'),
+  header = document.querySelector('header'),
+  mobileNav = document.querySelector('.mobile-nav');
+hamburgerBtn.addEventListener('click', function (event) {
+  console.log('working');
+  hamburgerClass.classList.toggle('close');
+  mobileNav.classList.toggle('show');
+})
+mobileNavLinks.forEach(function (mobileNavLink) {
+  mobileNavLink.addEventListener('click', function (event) {
+    let checkShow = mobileNav.classList.contains('show');
+    console.log(checkShow)
+    if (checkShow) {
+      hamburgerClass.classList.toggle('close');
+      mobileNav.classList.remove('show');
+    }
+  })
 })

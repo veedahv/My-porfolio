a7afcaf4aee1e2a40f80571608b45534fee73270
veedahv <veedahv55@gmail.com>
2021-10-27 23:53:05 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -17,24 +17,7 @@ const mobileNavLinks = document.querySelectorAll('.mobile-nav-link'),
   certificateBtns = document.querySelectorAll('.certificate-btn');
 
 
-
-// Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyCycw1mAfqDNNtBEA1NzVjgcGSfreeaspY",
-    authDomain: "portfolio-form-1fbdb.firebaseapp.com",
-    databaseURL: "portfolio-form-1fbdb-default-rtdb.firebaseio.com",
-    projectId: "portfolio-form-1fbdb",
-    storageBucket: "portfolio-form-1fbdb.appspot.com",
-    messagingSenderId: "781619757844",
-    appId: "1:781619757844:web:59d48903eed9bffe5eb801",
-    measurementId: "G-ECM5E149QR"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-var messagesRef = firebase.database().ref('messages');
+let certBtn;
 
 const preLoadAdd = () => {
   loader.style.visibility = 'visible';
@@ -57,10 +40,10 @@ const resizeHeight = () => {
 }
 resizeHeight();
 // window.onresize = resizeHeight();
-window.onresize = function() {
-          aboutCards.forEach(aboutCard => {
-            aboutCard.style.height = getComputedStyle(aboutCard).width;
-          });
+window.onresize = function () {
+  aboutCards.forEach(aboutCard => {
+    aboutCard.style.height = getComputedStyle(aboutCard).width;
+  });
 }
 
 const isInViewport = (el) => {
@@ -80,6 +63,27 @@ const isInViewport = (el) => {
     })
   }
 }
+const isCertInside = function (event) {
+  if (certificateShow.style.display == 'flex') {
+    if (event.target.classList.contains('certificate-btn')) {
+      certBtn = event.target;
+      // console.log(event.target);
+      // console.log(event.target.classList);
+    }
+    let isClickInsideCertificate = certificateShowImg.contains(event.target);
+    let isCertificateBtn = certBtn.contains(event.target);
+    if (!isCertificateBtn && !isClickInsideCertificate) {
+      certificateShow.style.display = 'none';
+      // console.log('lalalayaya');
+      document.removeEventListener('click', isCertInside);
+    // } else {
+      // console.log('ayayllayay');
+    }
+  // } else {
+  //   console.log('llayayayayoooo');
+  //   document.removeEventListener('click', isCertInside);
+  }
+}
 navLinks.forEach(navLink => {
   navLink.addEventListener('click', () => {
     navigationCheckbox.checked = false;
@@ -92,36 +96,8 @@ certificateBtns.forEach(certificateBtn => {
     viewCertificate.src = getImg.src;
     viewCertificate.alt = getImg.alt;
     certificateShow.style.display = 'flex';
-    if (certificateBtn.disabled === true) {
-      document.querySelector('.no-certificate-toast').style.animation = `inOut 5s ease-in-out`;
-      setTimeout(() => {
-        document.querySelector('.no-certificate-toast').style.animation = `none`;        
-      }, 5000);
-      console.log('lalala yaya');
-    } else {
-      console.log('lalala');
-    }
+    document.addEventListener('click', isCertInside);
   })
-})
-document.addEventListener('click', (event) => {
-  // let y = 0;
-  // for (let y = 0; y < certificateBtns.length; y++) {
-  //   const element = certificateBtns[y];
-    
-  // }
-
-    let isClickInside0 = certificateBtns[0].contains(event.target);
-    let isClickInside1 = certificateBtns[1].contains(event.target);
-    let isClickInside2 = certificateBtns[2].contains(event.target);
-    // let isClickInside3 = certificateBtns[3].contains(event.target);
-    // let isClickInside4 = certificateBtns[4].contains(event.target);
-    let isClickInsideCertificate = certificateShowImg.contains(event.target);
-  
-      
-      // if (!isClickInside0 && !isClickInside1 && !isClickInside2 && !isClickInside3 && !isClickInside4 && !isClickInsideCertificate) {
-      if (!isClickInside0 && !isClickInside1 && !isClickInside2 && !isClickInsideCertificate) {
-        certificateShow.style.display = 'none';   
-      }
 });
 document.addEventListener('scroll', () => {
   sections.forEach((section) => {
@@ -135,13 +111,24 @@ const showError = (message) => {
   formGroup.querySelector('small').innerText = message;
 };
 const saveMessage = () => {
-  let newMessageRef = messagesRef.push();
-   newMessageRef.set({
-     name: name.value,
-     email: email.value,
-     subject: subject.value,
-     message: message.value,
-   });
+  //  fetch('http://localhost:8081/', {
+  fetch('https://portfolio-message.herokuapp.com/', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name.value,
+      email: email.value,
+      subject: subject.value,
+      message: message.value,
+    })
+  }).then((res) => {
+    return res;
+    //  console.log(res);
+  }).then((res) => {
+    console.log(res);
+  })
 };
 
 function validateEmail(emailValid) {
@@ -150,14 +137,14 @@ function validateEmail(emailValid) {
   if (emailValidTest === true) {
     saveMessage();
     showError('');
-  email.style.marginBottom = "12px";
-  form.querySelectorAll('input').forEach(inputTag => {
-    inputTag.value = '';
-  });
-  form.querySelector('textarea').value = '';
-} else {
-  showError('Email invalid');
-  email.style.marginBottom = "0px";
+    email.style.marginBottom = "12px";
+    form.querySelectorAll('input').forEach(inputTag => {
+      inputTag.value = '';
+    });
+    form.querySelector('textarea').value = '';
+  } else {
+    showError('Email invalid');
+    email.style.marginBottom = "0px";
   }
 }
 

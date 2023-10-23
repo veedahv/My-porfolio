0f5d8821fae49bf382d9f31527d8927d42f82e7c
Victoria Akpan <veedahv55@gmail.com>
2023-10-23 17:33:38 +0100

    work experience updated

--- js/script.js
+++ js/script.js
@@ -20,6 +20,24 @@ const mobileNavLinks = document.querySelectorAll('.mobile-nav-link'),
 
 let certBtn;
 
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCycw1mAfqDNNtBEA1NzVjgcGSfreeaspY",
+    authDomain: "portfolio-form-1fbdb.firebaseapp.com",
+    databaseURL: "portfolio-form-1fbdb-default-rtdb.firebaseio.com",
+    projectId: "portfolio-form-1fbdb",
+    storageBucket: "portfolio-form-1fbdb.appspot.com",
+    messagingSenderId: "781619757844",
+    appId: "1:781619757844:web:59d48903eed9bffe5eb801",
+    measurementId: "G-ECM5E149QR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+var messagesRef = firebase.database().ref('messages');
+
 const preLoadAdd = () => {
   loader.style.visibility = 'visible';
   containAll.style.visibility = 'hidden';
@@ -142,31 +160,88 @@ const showError = (message) => {
   const formGroup = email.parentElement.closest('.input-wrap');
   formGroup.querySelector('small').innerText = message;
 };
+// const saveMessage = () => {
+//   //  fetch('http://localhost:8081/', {
+//   fetch('https://portfolio-message.herokuapp.com/', {
+//     method: 'post',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       name: name.value,
+//       email: email.value,
+//       subject: subject.value,
+//       message: message.value,
+//     })
+//   }).then((res) => {
+//     return res;
+//     //  console.log(res);
+//   }).then((res) => {
+//     console.log(res);
+//     if (res.status == 200) {
+      
+//     } else {
+      
+//     }
+//   }).catch(err => console.log(err))
+// };
+
+
+// certificateBtns.forEach(certificateBtn => {
+//   certificateBtn.addEventListener('click', () => {
+//     let getImg = certificateBtn.closest('.card-body').querySelector('img');
+//     viewCertificate.src = getImg.src;
+//     viewCertificate.alt = getImg.alt;
+//     certificateShow.style.display = 'flex';
+//     if (certificateBtn.disabled === true) {
+//       document.querySelector('.no-certificate-toast').style.animation = `inOut 5s ease-in-out`;
+//       setTimeout(() => {
+//         document.querySelector('.no-certificate-toast').style.animation = `none`;        
+//       }, 5000);
+//       console.log('lalala yaya');
+//     } else {
+//       console.log('lalala');
+//     }
+//   })
+// })
+// document.addEventListener('click', (event) => {
+//   // let y = 0;
+//   // for (let y = 0; y < certificateBtns.length; y++) {
+//   //   const element = certificateBtns[y];
+    
+//   // }
+
+//     let isClickInside0 = certificateBtns[0].contains(event.target);
+//     let isClickInside1 = certificateBtns[1].contains(event.target);
+//     let isClickInside2 = certificateBtns[2].contains(event.target);
+//     // let isClickInside3 = certificateBtns[3].contains(event.target);
+//     // let isClickInside4 = certificateBtns[4].contains(event.target);
+//     let isClickInsideCertificate = certificateShowImg.contains(event.target);
+  
+      
+//       // if (!isClickInside0 && !isClickInside1 && !isClickInside2 && !isClickInside3 && !isClickInside4 && !isClickInsideCertificate) {
+//       if (!isClickInside0 && !isClickInside1 && !isClickInside2 && !isClickInsideCertificate) {
+//         certificateShow.style.display = 'none';   
+//       }
+// });
+
 const saveMessage = () => {
-  //  fetch('http://localhost:8081/', {
-  fetch('https://portfolio-message.herokuapp.com/', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: name.value,
-      email: email.value,
-      subject: subject.value,
-      message: message.value,
-    })
-  }).then((res) => {
-    return res;
-    //  console.log(res);
-  }).then((res) => {
-    console.log(res);
-    if (res.status == 200) {
-      
-    } else {
-      
-    }
-  }).catch(err => console.log(err))
+  let newMessageRef = messagesRef.push();
+   newMessageRef.set({
+     name: name.value,
+     email: email.value,
+     subject: subject.value,
+     message: message.value,
+   });
 };
+
+
+
+
+
+
+
+
 
 function validateEmail(emailValid) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
